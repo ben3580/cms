274e3422182564c53291a1ccab39ff7586c36694
Subu Kandaswamy <b.kandaswamy@wsu.edu>
2023-03-15 16:44:15 -0700

    user model, login and sessions

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
+
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var coursesRouter = require('./routes/courses');
 
 var app = express();
 
@@ -19,8 +21,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'wsu489',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/courses', coursesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ db.js
@@ -0,0 +1,28 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database/cmsdb.sqlite');
+
+let sql = `SELECT * FROM USER WHERE username=? and password=?`;
+
+
+
+function getRow(sql, values) {
+    return new Promise((resolve, reject) =>{
+        db.get(sql, values, (err, row) => {
+            if (err) {
+              reject(err);
+            }else{
+              resolve(row);
+            }
+          });
+    })
+}
+
+async function getRowTest() {
+ let row = await getRow(sql,['subu', '1234'])
+ console.log(row) 
+}
+
+getRowTest().then(()=>console.log('query completed'))
+
+module.exports = { getRow }
+
